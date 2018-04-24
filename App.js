67a2d9e37384937f6cb8f1bfab8897053d86f731
Dmidma <d.oussema.d@gmail.com>
2018-04-24 22:45:28 +0100

    Added status in App

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
 import NewDeck from './components/NewDeck'
 import Decks from './components/Decks'
 
@@ -19,6 +20,10 @@ export default class App extends React.Component {
   render() {
     return (
         <View style={{ flex: 1 }}>
+        <View style={{height: Constants.statusBarHeight}}>
+            <StatusBar translucent backgroundColor='#ff4562'/>
+        </View>
+
             <Tabs />
         </View>
     );
